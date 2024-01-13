1201ec6a7863a67a08e49a8fc6383b5ad87b5f10
Sandesh Pokhrel <yespokhrel999@gmail.com>
2024-01-13 22:52:55 +0545

    finale done

--- fend/fendui/src/Pages/MyDocs.jsx
+++ fend/fendui/src/Pages/MyDocs.jsx
@@ -4,44 +4,49 @@ import API_EP from '../utils/ApiEndPoint';
 import { axiosPrivate } from '../api/axios/axios';
 import { File } from '../component/File';
 import './MyDocs.css'
-const files =[
-    {id:1,filename: "hellow", URL: "https://localhost:3000/"},
-    {id:2,filename: "ram" ,URL: "https://localhost:3000/"},
-    {id:3,filename: "sita" ,URL: "https://localhost:3000/"},
-    {id:4,filename: "adf" ,URL: "https://localhost:3000/"},
-    {id:5,filename: "fff" ,URL: "https://localhost:3000/"}
-]
+import useAxiosPrivate from '../packages/Auth/useAxiosPrivate';
+import { useState } from 'react';
+// const files =[
+//     {id:1,filename: "hellow", URL: "https://localhost:3000/"},
+//     {id:2,filename: "ram" ,URL: "https://localhost:3000/"},
+//     {id:3,filename: "sita" ,URL: "https://localhost:3000/"},
+//     {id:4,filename: "adf" ,URL: "https://localhost:3000/"},
+//     {id:5,filename: "fff" ,URL: "https://localhost:3000/"}
+// ]
 export const MyDocs = () => {
-    // useEffect(()=>{
-    //     let isMounted = true;
-    //     const controller = new AbortController();
-    //     const getDocuments = async () =>{
-    //         try{    
-    //             const res = await axiosPrivate.get(`${API_EP.SECTIONS}${props.id}/chapters/`,{ signal: controller.signal});
-    //             console.log(res.data);
-    //             isMounted && setDocuments(res.data);
-    //         }catch(err){
-    //             console.error(err);
-    //         }
-    //     }
+    const axiosPrivate = useAxiosPrivate();
+    const [doc, setDoc] = useState(null);
+    useEffect(()=>{
+        let isMounted = true;
+        const controller = new AbortController();
+        const getDocuments = async () =>{
+            try{    
+                const res = await axiosPrivate.get(`${API_EP.AUDIOPROCESS}`,{ signal: controller.signal});
+                console.log(res.data);
+                isMounted && setDoc(res.data);
+            }catch(err){
+                console.error(err);
+            }
+        }
         
-    //     getDocuments();
-    //     return() => {
-    //         isMounted = false;
-    //         controller.abort();
-    //     }
-    // },[]);
+        getDocuments();
+        return() => {
+            isMounted = false;
+            controller.abort();
+        }
+    },[]);
+
   return (
-    <>
+    <div className='docx'>
     <NavBar/>
     <div className="files-container">
-    {files.map((file)=>(
-        <File key = {file.id} filename = {file.filename} url = {file.URL}/>
+    {doc && doc.map((file)=>(
+        <File key = {file.id} filename = {file.doc_name} url = {file.document}/>
     ))}
     {/* <div>MyDocs</div> */}
     </div>
     
-    </>
+    </div>
     
   )
 }
